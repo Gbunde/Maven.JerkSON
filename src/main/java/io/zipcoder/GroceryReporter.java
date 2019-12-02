875e6eb002abb447254cebd830f1d52f6148bb44
Gbunde <56592891+Gbunde@users.noreply.github.com>
2019-12-02 16:57:11 -0500

    23 tests passing

--- src/main/java/io/zipcoder/GroceryReporter.java
+++ src/main/java/io/zipcoder/GroceryReporter.java
@@ -1,6 +1,11 @@
 package io.zipcoder;
 
 import io.zipcoder.utils.FileReader;
+import io.zipcoder.utils.Item;
+
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
 
 public class GroceryReporter {
     private final String originalFileText;
@@ -9,8 +14,34 @@ public class GroceryReporter {
         this.originalFileText = FileReader.readFile(jerksonFileName);
     }
 
+
     @Override
     public String toString() {
+        String[] names = {"Milk", "Bread", "Cookies", "Apples"};
+        HashMap<String, Double[]> prices = new HashMap<>();
+        ItemParser ip = new ItemParser();
+        List<Item> items = ip.parseItemList(originalFileText);
+        for (String name: names) {
+            long count = addGroceryItem(name, prices, items);
+            System.out.printf("name:%8s\tseen:%d\n", name, count);
+            System.out.println("=============\t=============");
+            Double max = prices.get(name)[0];
+            Double min = prices.get(name)[1];
+            System.out.printf("Price:%7.2f\n", max);
+            System.out.println("-------------\t------------");
+            if (!min.equals(max)) System.out.printf("Price:%7.2f\n", min);
+        }
+        System.out.println(ip.getErrorCount());
         return null;
+    }
+
+    public long addGroceryItem(String name, HashMap<String, Double[]> list, List<Item> items) {
+        Item max = items.stream().filter(item -> item.getName().equals(name.toLowerCase()))
+                .max(Comparator.comparingDouble(Item::getPrice)).get();
+        Item min = items.stream().filter(item -> item.getName().equals(name.toLowerCase()))
+                .min(Comparator.comparingDouble(Item::getPrice)).get();
+        Double[] counts = {max.getPrice(), min.getPrice()};
+        list.put(name, counts);
+        return items.stream().filter(item -> item.getName().equals(name.toLowerCase())).count();
     }
 }
--- src/main/java/io/zipcoder/ItemParser.java
+++ src/main/java/io/zipcoder/ItemParser.java
@@ -3,14 +3,52 @@ package io.zipcoder;
 import io.zipcoder.utils.Item;
 import io.zipcoder.utils.ItemParseException;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class ItemParser {
+    private Integer errorCount;
+
+    public ItemParser() {
+        errorCount = 0;
+    }
+
     public List<Item> parseItemList(String valueToParse) {
-        return null;
+        List<Item> items = new ArrayList<>();
+        Pattern p = Pattern.compile("##");
+        Arrays.stream(p.split(valueToParse)).forEach(str -> {
+            try {
+                items.add(parseSingleItem(str));
+            } catch (ItemParseException ex) {
+                errorCount++;
+            }
+        });
+        return items;
     }
 
     public Item parseSingleItem(String singleItem) throws ItemParseException {
-        return null;
+        Pattern p1 = Pattern.compile(";");
+        Pattern p2 = Pattern.compile("[:@^*%]");
+        Pattern p3 = Pattern.compile("##");
+        String[] rawPairs = p1.split(singleItem);
+        List<String[]> pairs = Arrays.stream(rawPairs).map(p2::split)
+                .collect(Collectors.toList());
+        try {
+            String name = pairs.get(0)[1].toLowerCase();
+            Double price = Double.valueOf(pairs.get(1)[1]);
+            String type = pairs.get(2)[1].toLowerCase();
+            String temp = pairs.get(3)[1];
+            String expiration = (p3.matcher(temp).find()) ? temp.substring(0, temp.length()-2) : temp;
+            return new Item(name, price, type, expiration);
+        } catch (Exception ex) {
+            throw new ItemParseException();
+        }
+    }
+
+    public Integer getErrorCount() {
+        return errorCount;
     }
 }
